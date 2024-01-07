9257631e1d16b1a28af03cc51ee3840f68d3258a
Vedpatel231 <vrp5108@psu.edu>
2024-01-07 15:47:44 -0500

    finished the project.

--- app.py
+++ app.py
@@ -6,28 +6,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Serve the index.html file
     return render_template('index.html')
 
 @app.route('/api/overall_aqi')
 def overall_aqi():
-    # Fetch and return overall AQI data
     data = notebook.get_overall_aqi()
     return jsonify(data)
 
 @app.route('/api/co_aqi')
 def co_aqi():
-    # Fetch and return CO AQI data
     data = notebook.get_co_aqi()
     return jsonify(data)
 
 @app.route('/api/ozone_aqi')
 def ozone_aqi():
-    # Fetch and return Ozone AQI data
     data = notebook.get_ozone_aqi()
     return jsonify(data)
 
-# ... Add similar routes for other AQI metrics ...
+@app.route('/api/no2_aqi')
+def no2_aqi():
+    data = notebook.get_no2_aqi()
+    return jsonify(data)
+
+@app.route('/api/pm25_aqi')
+def pm25_aqi():
+    data = notebook.get_pm25_aqi()
+    return jsonify(data)
+
+@app.route('/api/avg_aqi_countries')
+def avg_aqi_countries():
+    countries = [
+        "India", 
+        "United States of America", 
+        "United Kingdom of Great Britain and Northern Ireland", 
+        "Russian Federation", 
+        "China"
+    ]
+    data = notebook.get_avg_aqi_for_countries(countries)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- notebook.py
+++ notebook.py
@@ -18,4 +18,15 @@ def get_co_aqi():
 def get_ozone_aqi():
     return get_top_5_by_metric('Ozone_AQI_Value')
 
-# Add similar functions for other AQI metrics
+def get_no2_aqi():
+    return get_top_5_by_metric('NO2_AQI_Value')
+
+def get_pm25_aqi():
+    return get_top_5_by_metric('PM2.5_AQI_Value')
+
+def get_avg_aqi_for_countries(countries):
+    pollution = load_data()
+    filtered_pollution = pollution[pollution['Country'].isin(countries)]
+    avg_aqi_per_country = filtered_pollution.groupby('Country')['AQI_Value'].mean().reset_index()
+    avg_aqi_per_country['AQI_Value'] = avg_aqi_per_country['AQI_Value'].round(2)
+    return avg_aqi_per_country.to_dict(orient='records')
